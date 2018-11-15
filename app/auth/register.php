3f3ef4546cced257a0b192af4a96849d1145102e
allthingsdev <allthingsdev2017@gmail.com>
2018-11-15 07:33:26 +0300

    ...

---
+++ app/auth/register.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Login V6</title>
+	
+</head>
+<body>
+	
+	<form action="register_validate.php" method="POST">
+  		First name:<br>
+  			<input type="text" name="firstname" value=""><br>
+  		Last name:<br>
+  			<input type="text" name="lastname" value=""><br><br>
+  		User name:<br>
+  			<input type="text" name="username" value=""><br><br>
+  		Password:<br>
+  			<input type="password" name="password" value=""><br>
+  		Confirm Password:<br>
+  			<input type="password" name="confirmpassword" value=""><br><br>
+  			
+  		<input type="submit" value="Submit">
+	</form>
+		<br><br>
+  		<a class="button" href="login_new.php">Login</a>
+</body>
+</html>
