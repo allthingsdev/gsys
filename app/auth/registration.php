14bd0bcf2b9cc093cf6241eb07f2a16fb81476f4
althingsweb <allthingsweb2017@gmail.com>
2018-11-17 19:50:39 +0300

    ..

--- app/auth/registration.php
+++ app/auth/registration.php
@@ -4,34 +4,41 @@ require $root_dir.'config/init.php';
 require $root_dir.'app/entities/User.php';
 
 // define variables and set to empty values
-$firstname = $lastname = $username = $password = "";
+$firstname = $lastname = $username = $password = $confirmpassword = "";
 
 if (filter_input(INPUT_SERVER, 'REQUEST_METHOD') == 'POST') {
     $firstname = validate_input(filter_input(INPUT_POST, 'firstname'));
     $lastname = validate_input(filter_input(INPUT_POST, 'lastname'));
     $username = validate_input(filter_input(INPUT_POST, 'username'));
     $password = validate_input(filter_input(INPUT_POST, 'password'));
-}
+    $confirmpassword = validate_input(filter_input(INPUT_POST, 'confirmpassword'));
 
-//TODO -->move to a validation class
-function validate_input($formdata) {
-    $data = htmlspecialchars(stripslashes(trim($formdata)));
-    return $data;
-}
+    //TODO -->move to a validation class
+    function validate_input($formdata) {
+        $data = htmlspecialchars(stripslashes(trim($formdata)));
+        return $data;
+    }
 
-    // Create and persist a new user
-    $user = (new entities\User())
-    ->setFirstName($firstname)
-    ->setLastName($lastname)
-    ->setUserName($username)
-    ->setPassword($password)
-    ->setCreatedOn(new DateTime())
-    ->setUpdatedOn(new DateTime());
+    if ($password == $confirmpassword) {
     
-    $em->persist($user);
-    
-    // Finally flush and execute the database transaction
-    $em->flush();
-    
-    header('Location: '.ROOT_PATH.'app/auth/login.php');
+        // Create and persist a new user
+        $user = (new entities\User())
+        ->setFirstName($firstname)
+        ->setLastName($lastname)
+        ->setUserName($username)
+        ->setPassword($password)
+        ->setCreatedOn(new DateTime())
+        ->setUpdatedOn(new DateTime());
+        
+        $em->persist($user);
+        
+        // Finally flush and execute the database transaction
+        $em->flush();
+        
+        header('Location: '.ROOT_PATH.'app/auth/login.php');
+    } else {
+        //TODO ...create error variables to be passed around
+        die('password do not match');
+    }
+}
 
